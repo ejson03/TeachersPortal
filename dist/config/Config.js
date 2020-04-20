c3f5d9c6966f440645d725d8083f5d8b53231940
ejson03 <elvisejsondsouza57@gmail.com>
2020-04-20 20:58:04 +0530

    added new environment

--- dist/config/Config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var db_1 = require("./db");
-exports.DB = db_1.DB;
-var session_1 = require("./session");
-exports.Session = session_1.Session;
-//# sourceMappingURL=Config.js.map
