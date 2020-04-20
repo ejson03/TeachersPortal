52665980ce259786290f740ab4bb194daac766c8
ejson03 <elvisejsondsouza57@gmail.com>
2020-04-20 14:37:59 +0530

    postgres setup added

--- dist/config/db.js
+++ dist/config/db.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.DB = {
     Dialect: "postgres",
     ProjectName: "teacherportal",
-    DatabaseName: "postgres",
-    Host: "localhost",
-    UserName: "postgres",
+    DatabaseName: "d7ra2glp760kah",
+    Host: "ec2-52-200-119-0.compute-1.amazonaws.com",
+    UserName: "imbjvirpczaeqz",
     Port: 5432,
-    Password: "postgres",
+    Password: "54c2ab61ed83dd1b1c382acf8a1ee28e624eb8f383c7c3539ec0889c5da3f7bb",
 };
 //# sourceMappingURL=db.js.map
