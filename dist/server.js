1a416fba1a52d9c3892b99acaaf71328b7df8fb2
ejson03 <elvisejsondsouza57@gmail.com>
2020-04-20 16:26:04 +0530

    checkout server now

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
-app_1.app.listen('https://teachers-portal.herokuapp.com');
+app_1.app.listen(8000, () => {
+    console.log("Server is started......");
+});
 process.on("SIGINT", function () {
     console.log("App Shutting Down");
 });
--- ts/server.ts
+++ ts/server.ts
@@ -1,7 +1,9 @@
 import * as config from './config/server'
 import {app} from "./app";
 
-app.listen('https://teachers-portal.herokuapp.com');
+app.listen(8000, () => {
+  console.log("Server is started......")
+});
 
 process.on("SIGINT", function() {
   console.log("App Shutting Down");
