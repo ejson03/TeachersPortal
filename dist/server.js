31b44e649ba6b5ff5f0cf5bf2f43473e08361b7c
ejson03 <elvisejsondsouza57@gmail.com>
2020-04-20 17:03:12 +0530

    checkout new port

--- dist/server.js
+++ dist/server.js
@@ -1,8 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
-app_1.app.listen(8000, () => {
-    console.log("Server is started......");
+const PORT = process.env.PORT || 3000;
+app_1.app.listen(PORT, () => {
+    console.log(`Our app is running on port ${PORT}`);
 });
 process.on("SIGINT", function () {
     console.log("App Shutting Down");
--- ts/server.ts
+++ ts/server.ts
@@ -1,8 +1,8 @@
-import * as config from './config/server'
 import {app} from "./app";
 
-app.listen(8000, () => {
-  console.log("Server is started......")
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
 
 process.on("SIGINT", function() {
