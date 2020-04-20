6f83f29bdeea1fe9d33aade2bc864142eaac36a5
ejson03 <elvisejsondsouza57@gmail.com>
2020-04-20 20:24:22 +0530

    heroku production setup altered

--- ts/config/db.ts
+++ ts/config/db.ts
@@ -1,12 +1,12 @@
 require('dotenv').config()
 
 export const DB = {
-  Host: String(process.env.DATABASE_HOST || "localhost"),
-  Port: Number(process.env.DATABASE_PORT || 5432),
-  UserName: String(process.env.DATABASE_USER_NAME || "postgres"),
-  Password: String(process.env.DATABASE_PASSWORD || "postgres"),
-  DatabaseName: String(process.env.DATABASE_NAME || "teacherportal"),
-  SSL: Boolean(String(process.env.DATABASE_SSL) == "true" || false)
+  Host: String("localhost" || process.env.DATABASE_HOST),
+  Port: Number(5432 || process.env.DATABASE_PORT),
+  UserName: String("postgres" || process.env.DATABASE_USER_NAME),
+  Password: String("postgres" || process.env.DATABASE_PASSWORD),
+  DatabaseName: String( "teacherportal" || process.env.DATABASE_NAME),
+  SSL: Boolean(false || String(process.env.DATABASE_SSL) === "true")
 };
 
 console.log(DB);
