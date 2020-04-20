7db3a1353c8852d11622ae36c4547a6984370e4c
ejson03 <elvisejsondsouza57@gmail.com>
2020-04-20 20:41:49 +0530

    heroku production setup altered

--- ts/config/db.ts
+++ ts/config/db.ts
@@ -1,13 +1,12 @@
 require('dotenv').config()
 
 export const DB = {
-  Host: String(process.env.DATABASE_HOST),
-  Port: Number(process.env.DATABASE_PORT),
-  UserName: String(process.env.DATABASE_USER_NAME),
-  Password: String(process.env.DATABASE_PASSWORD),
-  DatabaseName: String(process.env.DATABASE_NAME),
-  SSL: Boolean(process.env.DATABASE_SSL)
+  Host: String(process.env.DATABASE_HOST || "localhost"),
+  Port: Number(process.env.DATABASE_PORT || 5432),
+  UserName: String(process.env.DATABASE_USER_NAME || "postgres"),
+  Password: String(process.env.DATABASE_PASSWORD || "postgres"),
+  DatabaseName: String(process.env.DATABASE_NAME || "teacherportal"),
+  SSL: Boolean(String(process.env.DATABASE_SSL) === "true" || false)
 };
 
 console.log(DB);
-console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
